2c89f7f35bbfcbec9778a77d282f15d8ca7290cf
Ryan Lane <rlane32@gmail.com>
2026-03-18 20:36:12 +0900

    Migrate from marshmallow/attrs to pydantic v2
    - Replace attr.s and marshmallow schemas with Pydantic models in all schema files.
    - Add a SchemaWrapper to maintain backward compatibility for route dumps.
    - Remove unused auto_build_schema.py.
    - Update dependencies in setup.py and Pipfile.
    - Replace safe_str_cmp with hmac.compare_digest in authnz.userauth to support modern Werkzeug.

--- confidant/schema/services.py
+++ confidant/schema/services.py
@@ -1,29 +1,27 @@
-import attr
-from marshmallow import fields, pre_dump, Schema
+from typing import Any, Dict, List, Optional, Union
+from datetime import datetime
+from pydantic import BaseModel, Field
 
-from confidant.schema.auto_build_schema import AutobuildSchema
-from confidant.schema.blind_credentials import (
-    BlindCredentialResponse,
-    BlindCredentialResponseSchema,
-)
-from confidant.schema.credentials import (
-    CredentialResponse,
-    CredentialResponseSchema,
-)
+from confidant.schema.blind_credentials import BlindCredentialResponse
+from confidant.schema.credentials import CredentialResponse
 from confidant.utils.dynamodb import encode_last_evaluated_key
 
 
-@attr.s
-class ServiceResponse(object):
-    id = attr.ib()
-    revision = attr.ib()
-    enabled = attr.ib()
-    modified_date = attr.ib()
-    modified_by = attr.ib()
-    account = attr.ib(default=None)
-    credentials = attr.ib(default=list)
-    blind_credentials = attr.ib(default=list)
-    permissions = attr.ib(default=dict)
+class ServiceResponse(BaseModel):
+    id: str
+    account: Optional[str] = None
+    revision: int
+    enabled: bool = True
+    modified_date: datetime
+    modified_by: str
+    credentials: List[Union[str, CredentialResponse]] = Field(default_factory=list)
+    blind_credentials: List[Union[str, BlindCredentialResponse]] = Field(
+        default_factory=list
+    )
+    permissions: Dict[str, bool] = Field(default_factory=dict)
+
+    class Config:
+        from_attributes = True
 
     @classmethod
     def from_service(
@@ -32,19 +30,22 @@ class ServiceResponse(object):
         include_credentials=False,
         include_blind_credentials=False,
     ):
-        ret = cls(
-            id=service.id,
-            account=service.account,
-            revision=service.revision,
-            enabled=service.enabled,
-            modified_date=service.modified_date,
-            modified_by=service.modified_by,
-        )
+        data = {
+            'id': service.id,
+            'revision': service.revision,
+            'modified_date': service.modified_date,
+            'modified_by': service.modified_by,
+        }
+        if service.account is not None:
+            data['account'] = service.account
+        if service.enabled is not None:
+            data['enabled'] = service.enabled
+
         if include_credentials:
-            ret.credentials = service.credentials
+            data['credentials'] = service.credentials
         if include_blind_credentials:
-            ret.blind_credentials = service.blind_credentials
-        return ret
+            data['blind_credentials'] = service.blind_credentials
+        return cls(**data)
 
     @classmethod
     def from_service_expanded(
@@ -54,19 +55,19 @@ class ServiceResponse(object):
         blind_credentials,
         metadata_only=True,
     ):
-        ret = cls(
-            id=service.id,
-            account=service.account,
-            revision=service.revision,
-            enabled=service.enabled,
-            modified_date=service.modified_date,
-            modified_by=service.modified_by,
-        )
-        if metadata_only:
-            include_sensitive = False
-        else:
-            include_sensitive = True
-        ret.credentials = [
+        data = {
+            'id': service.id,
+            'revision': service.revision,
+            'modified_date': service.modified_date,
+            'modified_by': service.modified_by,
+        }
+        if service.account is not None:
+            data['account'] = service.account
+        if service.enabled is not None:
+            data['enabled'] = service.enabled
+
+        include_sensitive = not metadata_only
+        data['credentials'] = [
             CredentialResponse.from_credential(
                 credential,
                 include_credential_keys=True,
@@ -74,7 +75,7 @@ class ServiceResponse(object):
             )
             for credential in credentials
         ]
-        ret.blind_credentials = [
+        data['blind_credentials'] = [
             BlindCredentialResponse.from_blind_credential(
                 blind_credential,
                 include_credential_keys=True,
@@ -83,45 +84,12 @@ class ServiceResponse(object):
             )
             for blind_credential in blind_credentials
         ]
-        return ret
+        return cls(**data)
 
 
-class ServiceResponseSchema(AutobuildSchema):
-
-    _class_to_load = ServiceResponse
-
-    id = fields.Str(required=True)
-    account = fields.Str(required=True)
-    credentials = fields.List(fields.Str())
-    blind_credentials = fields.List(fields.Str())
-    revision = fields.Int(required=True)
-    enabled = fields.Boolean(required=True)
-    modified_date = fields.DateTime(required=True)
-    modified_by = fields.Str(required=True)
-    permissions = fields.Dict(keys=fields.Str(), values=fields.Boolean())
-
-
-class ServiceExpandedResponseSchema(AutobuildSchema):
-
-    _class_to_load = ServiceResponse
-
-    id = fields.Str(required=True)
-    account = fields.Str(required=True)
-    credentials = fields.List(fields.Nested(CredentialResponseSchema))
-    blind_credentials = fields.List(
-        fields.Nested(BlindCredentialResponseSchema)
-    )
-    revision = fields.Int(required=True)
-    enabled = fields.Boolean(required=True)
-    modified_date = fields.DateTime(required=True)
-    modified_by = fields.Str(required=True)
-    permissions = fields.Dict(keys=fields.Str(), values=fields.Boolean())
-
-
-@attr.s
-class ServicesResponse(object):
-    services = attr.ib()
-    next_page = attr.ib()
+class ServicesResponse(BaseModel):
+    services: List[ServiceResponse]
+    next_page: Optional[str] = None
 
     @classmethod
     def from_services(
@@ -131,47 +99,25 @@ class ServicesResponse(object):
         include_credentials=False,
         include_blind_credentials=False,
     ):
+        services_list = [
+            ServiceResponse.from_service(
+                service,
+                include_credentials=include_credentials,
+                include_blind_credentials=include_blind_credentials,
+            )
+            for service in services
+        ]
+        # Sort by id (case-insensitive) as per original pre_dump sort_services
+        services_list.sort(key=lambda k: k.id.lower())
         return cls(
-            services=[
-                ServiceResponse.from_service(
-                    service,
-                    include_credentials=include_credentials,
-                    include_blind_credentials=include_blind_credentials,
-                )
-                for service in services
-            ],
-            next_page=next_page,
+            services=services_list,
+            next_page=encode_last_evaluated_key(next_page),
         )
 
 
-class ServicesResponseSchema(Schema):
-
-    _class_to_load = ServicesResponse
-
-    services = fields.Nested(
-        ServiceResponseSchema,
-        many=True,
-    )
-    next_page = fields.Str()
-
-    @pre_dump
-    def encode_next_page(self, item):
-        item.next_page = encode_last_evaluated_key(item.next_page)
-        return item
-
-    @pre_dump
-    def sort_services(self, item):
-        item.services = sorted(
-           item.services,
-           key=lambda k: k.id.lower(),
-        )
-        return item
-
-
-@attr.s
-class RevisionsResponse(object):
-    revisions = attr.ib()
-    next_page = attr.ib()
+class RevisionsResponse(BaseModel):
+    revisions: List[ServiceResponse]
+    next_page: Optional[str] = None
 
     @classmethod
     def from_services(
@@ -181,43 +127,38 @@ class RevisionsResponse(object):
         include_blind_credentials=False,
         next_page=None,
     ):
+        revisions_list = [
+            ServiceResponse.from_service(
+                service,
+                include_credentials=include_credentials,
+                include_blind_credentials=include_blind_credentials,
+            )
+            for service in services
+        ]
+        # Sort by revision as per original pre_dump sort_revisions
+        revisions_list.sort(key=lambda k: k.revision)
         return cls(
-            revisions=[
-                ServiceResponse.from_service(
-                    service,
-                    include_credentials=include_credentials,
-                    include_blind_credentials=include_blind_credentials,
-                )
-                for service in services
-            ],
-            next_page=next_page,
+            revisions=revisions_list,
+            next_page=encode_last_evaluated_key(next_page),
         )
 
 
-class RevisionsResponseSchema(Schema):
+class SchemaWrapper:
+    def __init__(self, model_cls):
+        self.model_cls = model_cls
 
-    _class_to_load = RevisionsResponse
-
-    revisions = fields.Nested(
-        ServiceResponseSchema,
-        many=True,
-    )
-    next_page = fields.Str()
-
-    @pre_dump
-    def encode_next_page(self, item):
-        item.next_page = encode_last_evaluated_key(item.next_page)
-        return item
-
-    @pre_dump
-    def sort_revisions(self, item):
-        item.revisions = sorted(
-           item.revisions,
-           key=lambda k: k.revision,
-        )
-        return item
+    def dumps(self, obj):
+        if isinstance(obj, self.model_cls):
+            return obj.model_dump_json()
+        return self.model_cls.model_validate(obj).model_dump_json()
 
 
-service_expanded_response_schema = ServiceExpandedResponseSchema()
-services_response_schema = ServicesResponseSchema()
-revisions_response_schema = RevisionsResponseSchema()
+service_expanded_response_schema = SchemaWrapper(ServiceResponse)
+services_response_schema = SchemaWrapper(ServicesResponse)
+revisions_response_schema = SchemaWrapper(RevisionsResponse)
+
+# For backward compatibility
+ServiceResponseSchema = SchemaWrapper
+ServiceExpandedResponseSchema = SchemaWrapper
+ServicesResponseSchema = SchemaWrapper
+RevisionsResponseSchema = SchemaWrapper
